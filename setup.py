b7cc9e940e571dc72d11f29279a0b6ab6a3c5d76
jnimmo <james@nimmo.net.nz>
2016-11-23 08:36:06 +1300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name='pyIntesisHome',
+      version='0.1',
+      description='A python3 library for running asynchronus communications with IntesisHome Smart AC Controllers',
+      url='https://github.com/jnimmo/pyIntesisHome',
+      download_url='https://github.com/jnimmo/pyIntesisHome/tarball/0.1',
+      author='James Nimmo',
+      author_email='james@nimmo.net.nz',
+      license='MIT',
+      packages=['pyIntesisHome'],
+      classifiers=['Development Status :: 3 - Alpha', 'Programming Language :: Python :: 3.4','Programming Language :: Python :: 3.5','Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator']
+)
