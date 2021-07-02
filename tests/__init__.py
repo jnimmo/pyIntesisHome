96bea51de432612e4c151bb4d501251cbfc682de
René Klomp <rene@klomp.ws>
2021-07-02 15:21:16 +0200

    Add first tests

---
+++ tests/__init__.py
@@ -0,0 +1,301 @@
+"""Tests for pyintesishome."""
+import pytest
+from aioresponses import aioresponses
+from aioresponses import CallbackResult
+
+MOCK_HOST = "1.1.1.1"
+MOCK_PASS = "password"
+MOCK_USER = "admin"
+MOCK_DEVICE_ID = "mock_dev_id"
+
+MOCK_VAL_POWER_STATE = 0
+MOCK_VAL_MODE = 4
+MOCK_VAL_FAN_SPEED = 1
+MOCK_VAL_VVANE = 2
+MOCK_VAL_HVANE = 3
+MOCK_VAL_SETPOINT = 210
+MOCK_VAL_TEMP = 240
+MOCK_VAL_RUN_HOURS = 567
+MOCK_VAL_ERROR = 0
+MOCK_VAL_MIN_SET = 180
+MOCK_VAL_MAX_SET = 300
+MOCK_VAL_OUT_TEMP = 260
+MOCK_VAL_PRESET = 1
+
+
+@pytest.fixture
+def mock_aioresponse():
+    """Yield a mock aioresponses."""
+    with aioresponses() as m:
+        yield m
+
+
+def local_api_callback(url, **kwargs):
+    req_json = kwargs.pop("json")
+    req_cmd = req_json["command"]
+    req_data = req_json["data"]
+
+    if req_cmd == "login":
+        if req_data["username"] == MOCK_USER and req_data["password"] == MOCK_PASS:
+            return CallbackResult(
+                status=200,
+                payload={
+                    "success": True,
+                    "data": {"id": {"sessionID": "lf1XbgHmapgwEjvpc2m8joB4KmREqkm"}},
+                },
+            )
+        else:
+            return CallbackResult(
+                status=200,
+                payload={
+                    "success": False,
+                    "data": None,
+                    "error": {"code": 5, "message": "Incorrect User name or password"},
+                },
+            )
+
+    if req_cmd == "getinfo":
+        return CallbackResult(
+            status=200,
+            payload={
+                "success": True,
+                "data": {
+                    "info": {
+                        "wlanSTAMAC": "CC:3F:1D:12:34:56",
+                        "wlanAPMAC": "CE:3F:1D:12:34:56",
+                        "ownSSID": "DEVICE_123456",
+                        "fwVersion": "1.4.7; 1.3.3; 1.5; 1.0.1.0",
+                        "wlanFwVersion": "1.2.0",
+                        "acStatus": 0,
+                        "wlanLNK": 1,
+                        "ssid": "Klomp IOT",
+                        "rssi": -53,
+                        "tcpServerLNK": 1,
+                        "localdatetime": "Fri Jul 02 12:01:12 +2:00 2021 DST",
+                        "powerStatus": 78,
+                        "wifiTxPower": 78,
+                        "lastconfigdatetime": 0,
+                        "deviceModel": "MH-AC-WIFI-1",
+                        "sn": MOCK_DEVICE_ID,
+                        "lastError": 0,
+                    }
+                },
+            },
+        )
+
+    if req_cmd == "getavailabledatapoints":
+        return CallbackResult(
+            status=200,
+            payload={
+                "success": True,
+                "data": {
+                    "dp": {
+                        "datapoints": [
+                            {
+                                "uid": 1,
+                                "rw": "rw",
+                                "type": 1,
+                                "descr": {"numStates": 2, "states": [0, 1]},
+                            },
+                            {
+                                "uid": 2,
+                                "rw": "rw",
+                                "type": 1,
+                                "descr": {"numStates": 5, "states": [0, 1, 2, 3, 4]},
+                            },
+                            {
+                                "uid": 4,
+                                "rw": "rw",
+                                "type": 1,
+                                "descr": {"numStates": 4, "states": [1, 2, 3, 4]},
+                            },
+                            {
+                                "uid": 5,
+                                "rw": "rw",
+                                "type": 1,
+                                "descr": {"numStates": 5, "states": [1, 2, 3, 4, 10]},
+                            },
+                            {
+                                "uid": 6,
+                                "rw": "rw",
+                                "type": 1,
+                                "descr": {"numStates": 5, "states": [1, 2, 3, 4, 10]},
+                            },
+                            {
+                                "uid": 9,
+                                "rw": "rw",
+                                "type": 2,
+                                "descr": {"maxValue": 300, "minValue": 180},
+                            },
+                            {
+                                "uid": 10,
+                                "rw": "r",
+                                "type": 2,
+                                "descr": {"maxValue": 500, "minValue": -100},
+                            },
+                            {
+                                "uid": 12,
+                                "rw": "rw",
+                                "type": 1,
+                                "descr": {"numStates": 2, "states": [0, 1]},
+                            },
+                            {"uid": 13, "rw": "rw", "type": 0, "descr": {}},
+                            {
+                                "uid": 14,
+                                "rw": "r",
+                                "type": 1,
+                                "descr": {"numStates": 2, "states": [0, 1]},
+                            },
+                            {"uid": 15, "rw": "r", "type": 3, "descr": {}},
+                            {
+                                "uid": 35,
+                                "rw": "r",
+                                "type": 2,
+                                "descr": {"maxValue": 300, "minValue": 180},
+                            },
+                            {
+                                "uid": 36,
+                                "rw": "r",
+                                "type": 2,
+                                "descr": {"maxValue": 300, "minValue": 180},
+                            },
+                            {
+                                "uid": 37,
+                                "rw": "r",
+                                "type": 2,
+                                "descr": {"maxValue": 430, "minValue": -250},
+                            },
+                            {
+                                "uid": 42,
+                                "rw": "r",
+                                "type": 1,
+                                "descr": {"numStates": 3, "states": [0, 1, 2]},
+                            },
+                            {"uid": 181, "rw": "rw", "type": 0, "descr": {}},
+                            {"uid": 182, "rw": "rw", "type": 0, "descr": {}},
+                            {"uid": 183, "rw": "rw", "type": 0, "descr": {}},
+                            {"uid": 184, "rw": "rw", "type": 0, "descr": {}},
+                        ]
+                    }
+                },
+            },
+        )
+
+    if req_cmd == "getdatapointvalue":
+        return CallbackResult(
+            status=200,
+            payload={
+                "success": True,
+                "data": {
+                    "dpval": [
+                        {"uid": 1, "value": MOCK_VAL_POWER_STATE, "status": 0},
+                        {"uid": 2, "value": MOCK_VAL_MODE, "status": 0},
+                        {"uid": 4, "value": MOCK_VAL_FAN_SPEED, "status": 0},
+                        {"uid": 5, "value": MOCK_VAL_VVANE, "status": 0},
+                        {"uid": 6, "value": MOCK_VAL_HVANE, "status": 0},
+                        {"uid": 9, "value": MOCK_VAL_SETPOINT, "status": 0},
+                        {"uid": 10, "value": MOCK_VAL_TEMP, "status": 0},
+                        {"uid": 12, "value": 0, "status": 0},
+                        {"uid": 13, "value": MOCK_VAL_RUN_HOURS, "status": 0},
+                        {"uid": 14, "value": 0, "status": 0},
+                        {"uid": 15, "value": MOCK_VAL_ERROR, "status": 0},
+                        {"uid": 35, "value": MOCK_VAL_MIN_SET, "status": 0},
+                        {"uid": 36, "value": MOCK_VAL_MAX_SET, "status": 0},
+                        {"uid": 37, "value": MOCK_VAL_OUT_TEMP, "status": 0},
+                        {"uid": 42, "value": MOCK_VAL_PRESET, "status": 0},
+                        {"uid": 181, "value": 0, "status": 0},
+                        {"uid": 182, "value": 0, "status": 0},
+                        {"uid": 183, "value": 0, "status": 0},
+                        {"uid": 184, "value": 0, "status": 0},
+                    ]
+                },
+            },
+        )
+
+    return CallbackResult(status=500)
+
+
+def cloud_api_callback(url, **kwargs):
+    req_data = kwargs["data"]
+    req_cmd = req_data["cmd"]
+    print(req_cmd)
+
+    payload = {}
+
+    if "status" in req_cmd:
+        payload["status"] = {
+            "hash": "7398e787639ab87c431f77b96e4a1590f16a4384",
+            "status": [
+                {"deviceId": MOCK_DEVICE_ID, "uid": 1, "value": MOCK_VAL_POWER_STATE},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 2, "value": MOCK_VAL_MODE},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 4, "value": MOCK_VAL_FAN_SPEED},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 5, "value": MOCK_VAL_VVANE},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 6, "value": MOCK_VAL_HVANE},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 9, "value": MOCK_VAL_SETPOINT},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 10, "value": MOCK_VAL_TEMP},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 12, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 13, "value": MOCK_VAL_RUN_HOURS},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 14, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 15, "value": MOCK_VAL_ERROR},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 34, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 35, "value": MOCK_VAL_MIN_SET},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 36, "value": MOCK_VAL_MAX_SET},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 37, "value": MOCK_VAL_OUT_TEMP},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 42, "value": MOCK_VAL_PRESET},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 54, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 61, "value": 63},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 62, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 63, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 64, "value": 1054},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 65, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 66, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 67, "value": 30},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50000, "value": 1},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50001, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50002, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50003, "value": 0},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50004, "value": 240},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50005, "value": 280},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50006, "value": 190},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50007, "value": 230},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50008, "value": 1},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50009, "value": 3},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 50010, "value": 255},
+                {"deviceId": MOCK_DEVICE_ID, "uid": 60002, "value": 204},
+            ],
+        }
+    if "config" in req_cmd:
+        payload["config"] = {
+            "token": 1234567890,
+            "pushToken": "channel-0123456789",
+            "lastAppVersion": "1.8.1",
+            "forceUpdate": 0,
+            "setDelay": 0.7,
+            "serverIP": "212.92.35.33",
+            "serverPort": 8210,
+            "hash": "ea4b71bd4e8ba5ad045e6bbbb4118d2816efbcb5",
+            "inst": [
+                {
+                    "id": 1,
+                    "order": 1,
+                    "name": "First installation",
+                    "devices": [
+                        {
+                            "id": MOCK_DEVICE_ID,
+                            "name": "MOCK DEVICE",
+                            "familyId": 4864,
+                            "modelId": 550,
+                            "installationId": 20151,
+                            "zoneId": 20168,
+                            "order": 1,
+                            "widgets": [15, 3, 5, 7, 17, 9, 13],
+                        },
+                    ],
+                },
+            ],
+        }
+
+    if payload:
+        return CallbackResult(status=200, payload=payload)
+    else:
+        return CallbackResult(status=500)
---
+++ tests/test_pyintesishome.py
@@ -0,0 +1,127 @@
+"""Tests for pyintesishome."""
+import asyncio
+import pytest
+
+from . import (
+    MOCK_VAL_RUN_HOURS,
+    cloud_api_callback,
+    mock_aioresponse,
+    local_api_callback,
+    MOCK_HOST,
+    MOCK_PASS,
+    MOCK_USER,
+    MOCK_DEVICE_ID,
+)  # noqa: F401
+from pyintesishome.const import DEVICE_INTESISHOME, DEVICE_INTESISHOME_LOCAL, API_URL
+from pyintesishome import IntesisHomeLocal, IntesisHome
+
+
+controllers = {}
+loop = asyncio.get_event_loop()
+
+
+async def async_setup_controllers():
+    # The aiohttp.ClientSession should be created within an async function
+    controllers["local"] = IntesisHomeLocal(
+        MOCK_HOST,
+        MOCK_USER,
+        MOCK_PASS,
+        loop=loop,
+        device_type=DEVICE_INTESISHOME_LOCAL,
+    )
+
+    controllers["cloud"] = IntesisHome(
+        MOCK_USER,
+        MOCK_PASS,
+        loop=loop,
+        device_type=DEVICE_INTESISHOME,
+    )
+
+
+loop.run_until_complete(async_setup_controllers())
+
+
+@pytest.mark.parametrize("controller", controllers.values(), ids=controllers.keys())
+class TestPyIntesisHome:
+    @pytest.fixture(autouse=True)
+    async def _setup(self, mock_aioresponse):
+        mock_aioresponse.post(
+            f"http://{MOCK_HOST}/api.cgi",
+            callback=local_api_callback,
+            repeat=True,
+        )
+
+        mock_aioresponse.post(
+            f"{API_URL[DEVICE_INTESISHOME]}",
+            callback=cloud_api_callback,
+            repeat=True,
+        )
+
+    async def test_connect(self, controller):
+        result = await controller.connect()
+        assert result == None
+
+    def test_get_power_state(self, controller):
+        result = controller.get_power_state(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "off"
+
+    def test_get_mode(self, controller):
+        result = controller.get_mode(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "cool"
+
+    def test_get_fan_speed(self, controller):
+        result = controller.get_fan_speed(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "quiet"
+
+    def test_get_vertical_swing(self, controller):
+        result = controller.get_vertical_swing(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "manual2"
+
+    def test_get_horizontal_swing(self, controller):
+        result = controller.get_horizontal_swing(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "manual3"
+
+    def test_get_setpoint(self, controller):
+        result = controller.get_setpoint(MOCK_DEVICE_ID)
+        assert isinstance(result, float)
+        assert result == 21.0
+
+    def test_get_temperature(self, controller):
+        result = controller.get_temperature(MOCK_DEVICE_ID)
+        assert isinstance(result, float)
+        assert result == 24.0
+
+    def test_get_run_hours(self, controller):
+        result = controller.get_run_hours(MOCK_DEVICE_ID)
+        assert isinstance(result, int)
+        assert result == MOCK_VAL_RUN_HOURS
+
+    def test_get_error(self, controller):
+        result = controller.get_error(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "H00: No abnormality detected"
+
+    def test_get_min_setpoint(self, controller):
+        result = controller.get_min_setpoint(MOCK_DEVICE_ID)
+        assert isinstance(result, float)
+        assert result == 18.0
+
+    def test_get_max_setpoint(self, controller):
+        result = controller.get_max_setpoint(MOCK_DEVICE_ID)
+        assert isinstance(result, float)
+        assert result == 30.0
+
+    def test_get_outdoor_temperature(self, controller):
+        result = controller.get_outdoor_temperature(MOCK_DEVICE_ID)
+        assert isinstance(result, float)
+        assert result == 26.0
+
+    def test_get_preset_mode(self, controller):
+        result = controller.get_preset_mode(MOCK_DEVICE_ID)
+        assert isinstance(result, str)
+        assert result == "eco"
