6aa2c665bcb340fe5a2f46d58e2459926c9a7942
kesawi <14970452+kesawi@users.noreply.github.com>
2026-05-17 21:26:03 +1000

    style: add blank line between methods for ruff format compliance

--- pyintesishome/intesishomelocal.py
+++ pyintesishome/intesishomelocal.py
@@ -334,6 +334,7 @@ class IntesisHomeLocal(IntesisBase):
             INTESIS_MAP[uid]["values"][i]
             for i in self._datapoints[uid]["descr"]["states"]
         ]
+
     def has_horizontal_swing(self, device_id) -> bool:
         """Entity supports horizontal swing."""
         return self._has_datapoint("hvane")
